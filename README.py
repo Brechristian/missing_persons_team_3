2c7e71e69ba0905ace43de7c341262811b1370d1
Brechristian <breshearschristian@hotmail.com>
2022-11-15 11:21:33 -0700

    .

---
+++ README.py
@@ -0,0 +1,5 @@
+# description: Purpose is to show ability to design and implement a website that will help the BYU community
+#              learn about and engage with combatting human trafficking.
+# Authors: Christian Breshears,
+# Hours worked per group member:
+# Christian: 3
--- test.py
+++
@@ -1,4 +0,0 @@
-# description:
-# Authors: Christian Breshears,
-# Hours worked per group member:
-# - Christian:
