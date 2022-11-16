bd38213ff071c54beb387dbbd8b9fc02de6a828b
cdawg1213 <103866953+cdawg1213@users.noreply.github.com>
2022-11-15 22:25:56 -0700

    Made some minor changes to the Index page

--- README.py
+++ README.py
@@ -1,5 +1,6 @@
 # description: Purpose is to show ability to design and implement a website that will help the BYU community
 #              learn about and engage with combatting human trafficking.
-# Authors: Christian Breshears,
+# Authors: Christian Breshears, Coleman Shuldberg
 # Hours worked per group member:
 # Christian: 5
+# Coleman: 2
