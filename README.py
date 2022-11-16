87b0192c6ee0fa3a5b1672450a98e6b6a3e3b219
Jerm2000 <jeremya1@byu.edu>
2022-11-16 13:58:15 -0700

    Added name to readme

--- README.py
+++ README.py
@@ -1,6 +1,7 @@
 # description: Purpose is to show ability to design and implement a website that will help the BYU community
 #              learn about and engage with combatting human trafficking.
-# Authors: Christian Breshears, Coleman Shuldberg
+# Authors: Christian Breshears, Coleman Shuldberg, Jeremy Abbott
 # Hours worked per group member:
 # Christian: 5
 # Coleman: 2
+# Jeremy: 1
