96e51d182ed8d636fef1a7cde948e38d6af33f3d
Andres Nino <andresfernando347@gmail.com>
2022-11-17 14:46:56 -0700

    last

--- README.py
+++ README.py
@@ -1,9 +1,10 @@
 # description: Purpose is to show ability to design and implement a website that will help the BYU community
 #              learn about and engage with combatting human trafficking.
-# Authors: Christian Breshears, Coleman Shuldberg, Jeremy Abbott, Sawyer Evans
+# Authors: Christian Breshears, Coleman Shuldberg, Jeremy Abbott, Sawyer Evans, Andres Nino
 # Hours worked per group member:
 # Christian: 5
 # Coleman: 2
 # Jeremy: 1
 # Sawyer: 0.5
 # Mckay: 0.5
+# Andres: 2.5
