5605ef79344b9ebabcb93bcd026355d92b69cd0e
Andres Nino <andresfernando347@gmail.com>
2022-11-22 12:04:12 -0700

    Created user and connected to database

--- missing_persons_team_3/settings.py
+++ missing_persons_team_3/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'missing_persons_team_3.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'missing_persons',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost' 
+
     }
 }
 
