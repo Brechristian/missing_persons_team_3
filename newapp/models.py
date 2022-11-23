ba2f915822fdcd619ff4a3e673ef2b5baeab4c63
Brechristian <breshearschristian@hotmail.com>
2022-11-22 18:22:33 -0700

    admin interface completion and database stuff

--- newapp/models.py
+++ newapp/models.py
@@ -9,11 +9,11 @@ class missing_person(models.Model):
     first_name = models.CharField(max_length=12)
     last_name = models.CharField(max_length=12)
     date_missing = models.CharField(max_length=11)
-    age_at_missing = models.CharField(max_length=2)
-    city = models.CharField(max_length=12)
-    state = models.CharField(max_length=2)
-    gender = models.CharField(max_length=1)
-    race = models.CharField(max_length=1)
+    # age_at_missing = models.CharField(max_length=2)
+    # city = models.CharField(max_length=12)
+    # state = models.CharField(max_length=2)
+    # gender = models.CharField(max_length=1)
+    # race = models.CharField(max_length=1)
 
     def __str__(self):
         return self.first_name, self.last_name
